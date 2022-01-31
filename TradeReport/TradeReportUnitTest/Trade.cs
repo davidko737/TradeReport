b9a22f31f72d246af8c6567f50ce09f6ce315fde
David Ko <davidko20160606@gmail.com>
2022-01-31 11:51:10 +0800

    [Update] Update Trade model for unit test

---
+++ TradeReport/TradeReportUnitTest/Trade.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TradeReportUnitTest
+{
+    public class Trade
+    {
+        public string TradeRef { get; set; }
+        public DateTime ExecutionDate { get; set; }
+        public decimal Qty { get; set; }
+        public string BuySell { get; set; }
+        public double Price { get; set; }
+        public string Desc { get; set; }
+        public int ProductId { get; set; }
+        public int BrokerId { get; set; }
+        public DateTime Maturity { get; set; }
+    }
+}
--- TradeReport/TradeReportUnitTest/UnitTest.cs
+++ TradeReport/TradeReportUnitTest/UnitTest.cs
@@ -267,17 +267,6 @@ namespace TradeReportUnitTest
             return productName;
         }
     }
-
-    public class Trade
-    {
-        public string TradeRef { get; set; }
-        public DateTime ExecutionDate { get; set; }
-        public decimal Qty { get; set; }
-        public string BuySell { get; set; }
-        public double Price { get; set; }
-        public string Desc { get; set; }
-        public int ProductId { get; set; }
-        public int BrokerId { get; set; }
-        public DateTime Maturity { get; set; }
-    }
 }
+
+
